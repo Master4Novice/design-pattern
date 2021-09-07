f816db171851eed5a38b43bdeb85f6afecd96a63
Abhinav Dwivedi <a.dwivedispu@gmail.com>
2021-09-07 13:43:21 +0530

    ESLint & Prettier Added

--- src/design-pattern/DesignPatternApp.jsx
+++ src/design-pattern/DesignPatternApp.jsx
@@ -1,17 +1,15 @@
-import React, { Component} from "react";
-import { hot } from "react-hot-loader";
-import "./DesignPatternApp.css";
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './DesignPatternApp.css';
 
 import Typography from '@material-ui/core/Typography';
 
-class DesignPatternApp extends Component{
-  render(){
-    return(
-      <div className="DPApp">
-        <Typography variant="h3">Design Pattern ES6+</Typography>
-      </div>
-    );
-  }
+function DesignPatternApp() {
+  return (
+    <div className="DPApp">
+      <Typography variant="h3">Design Pattern ES6+</Typography>
+    </div>
+  );
 }
 
 export default hot(module)(DesignPatternApp);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MomentUtils from '@date-io/moment';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-import DesignPatternApp from "./design-pattern/DesignPatternApp";
-import theme from "./theme.tsx";
-import { ThemeProvider } from "@material-ui/core/styles";
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import MomentUtils from '@date-io/moment';
+import DesignPatternApp from './design-pattern/DesignPatternApp';
+import theme from './theme.tsx';
 
-ReactDOM.render(<ThemeProvider theme={theme}><MuiPickersUtilsProvider utils={MomentUtils}><DesignPatternApp /></MuiPickersUtilsProvider></ThemeProvider>, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <DesignPatternApp />
+    </MuiPickersUtilsProvider>
+  </ThemeProvider>,
+  document.getElementById('root')
+);
